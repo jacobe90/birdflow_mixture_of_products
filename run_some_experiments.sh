8df74f3021dfe55b8882bb9fdbbb487321236513
jacobe90 <jacobepstein90@gmail.com>
2023-07-31 13:25:12 +0000

    unity merge

--- run_some_experiments.sh
+++ run_some_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -c 2
 #SBATCH --mem=20GB
-#SBATCH -p gypsum-1080ti
+#SBATCH -p gpu
 #SBATCH -G 1
-#SBATCH -t 30:00:00
+#SBATCH -t 24:00:00
 #SBATCH -o slurm-%x.%j.out
 
 /home/jepstein_umass_edu/.conda/envs/birdflow_two/bin/python experiments.py
